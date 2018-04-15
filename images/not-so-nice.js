dda2999ad61ba761e82b18e7be51107e185d20a2
Elijah Grubb <u0894728@utah.edu>
2018-04-15 01:36:06 -0600

    more swam

--- images/not-so-nice.js
+++ images/not-so-nice.js
@@ -4,14 +4,14 @@
 		for (var playerID in Players.getIDs()) {
 			var player = Players.get(playerID);
 			if (player.type == 5 && player.team != me.team) {
-				console.log(player.lowResPos);
+				console.log(player.losResPos);
 			}
 		}
 	});
 
 	SWAM.registerExtension({
 		name: "not-so-nice",
-		id: "not-so-nice",
+		id: "notsonice",
 		description: "this is probably a bad idea",
 		author: "jello",
 		version: "0.1"
